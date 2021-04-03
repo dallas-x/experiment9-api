c09a68f91b9768583c900a853e08ecf7eb6a9c41
Dallas Baker <dallas.baker@sheldyn.io>
2021-04-03 14:55:21 -0700

    chore(dev): cleanup 20210402

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import chalk from 'chalk';
 import cors from 'cors';
-import path from 'path';
 import uploadRouter from './src/routes/uploadRoute';
 import statsRouter from './src/routes/statsRoute';
 const debug = require('debug')('app');
@@ -14,7 +12,6 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.use(uploadRouter);
 app.use(statsRouter);
